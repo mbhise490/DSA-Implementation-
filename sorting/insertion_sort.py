425e6535663342375274b07de0c74c729d1923e8
Mahesh Bhise <mbhise490@gmail.com>
2026-04-28 19:31:18 +0530

    insertion sort added

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,16 @@
+
+def insertion_sort(arr):
+    n=len(arr)
+    for i in range(1,n):
+        c_value=arr[i]
+        p=i
+        while p>0 and arr[p-1]>c_value:
+
+            arr[p]=arr[p-1]
+            p=p-1
+        arr[p]=c_value
+    return arr
+
+arr=[3,4,2,4,3,4,2,3,42,3]
+
+print(insertion_sort(arr))
